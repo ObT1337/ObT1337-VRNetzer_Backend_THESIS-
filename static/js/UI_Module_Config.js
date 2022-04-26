e99b7efdaf73084dba3b1e9e49f7fda07c2a6046
sebastian <sebastian@OMEN.local>
2022-04-26 16:37:39 +0200

    Update UI_Module_Config.js

--- static/js/UI_Module_Config.js
+++ static/js/UI_Module_Config.js
@@ -2,5 +2,5 @@
 //var dbprefix = 'http://vrnetzer.westeurope.cloudapp.azure.com:8887';
 
 var dbprefix = 'http://127.0.0.1:5000';
-var dbuser = 'steveballmer';
-var dbpw = 'code peaceful canon shorter';
+//var dbuser = 'steveballmer';
+//var dbpw = 'code peaceful canon shorter';
