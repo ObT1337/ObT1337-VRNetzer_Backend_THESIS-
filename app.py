7d9e90acd9edd8a6cd5aa97b9054bd2e1288cead
ObT1337 <a12048115@unet.univie.ac.at>
2022-11-04 13:58:09 +0100

    Corrected problems in the nodepanel route updated nodes file of new_ppi

--- app.py
+++ app.py
@@ -398,15 +398,15 @@ def nodepanel():
     project = request.args.get("project")
     if project is None:
         project = "new_ppi"
-        folder = os.path.join("static", "projects", project)
-        with open(os.path.join(folder, "pfile.json"), "r") as json_file:
-            global pfile
-            pfile = json.load(json_file)
 
-    if project:
-        folder = os.path.join("static", "projects", project)
-        with open(os.path.join(folder, "nodes.json"), "r") as json_file:
-            nodes = json.load(json_file)
+    folder = os.path.join("static", "projects", project)
+    with open(os.path.join(folder, "pfile.json"), "r") as json_file:
+        global pfile
+        pfile = json.load(json_file)
+
+    with open(os.path.join(folder, "nodes.json"), "r") as json_file:
+        nodes = json.load(json_file)
+
     add_key = "NA"  # Additional key to show under Structural Information
     # nodes = {node["id"]: node for node in nodes}
     global sessionData
