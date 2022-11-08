f79ceea8a9ff99c64525d059e90bcea445dc941d
ObT1337 <a12048115@unet.univie.ac.at>
2022-11-08 13:15:30 +0100

    Added Route to get structure scales corrected port on app.py

--- app.py
+++ app.py
@@ -271,6 +271,46 @@ def nodeinfo():
         return nodes["nodes"][int(id)]
 
 
+@app.route("/scale", methods=["GET"])
+def get_structure_scale() -> float or str:
+    """Return the scale of the structure as a float. If the structure is not found (or not provided), the size file is not available or the mode is not given, the function will return an error message as string. To provide the UniProtID add the 'uniprot=<UniProtID>', for the mode add 'mode=<mode>' to the URL. Currently available modes are 'cartoon' and 'electrostatic'. The default mode is 'cartoon'."""
+
+    uniprot = request.args.get("uniprot")
+    mode = request.args.get("mode")
+
+    if mode is None:
+        print("Error: No mode provided. Will use default mode 'cartoon'.")
+        mode = "cartoon"
+
+    if uniprot is None:
+        return "Error: No UniProtID provided."
+
+    possible_files = {
+        "cartoon": os.path.join(".", "static", "csv", "scales_Cartoon.csv"),
+        "electrostatic": os.path.join(
+            ".", "static", "csv", "scales_electrostatic_surface.csv"
+        ),
+    }
+    scale_file = possible_files.get(mode)
+
+    # Prevent FileNotFound errors.
+    if scale_file is None:
+        return "Error: Mode not available."
+    if not os.path.exists(scale_file):
+        return "Error: File not found."
+
+    # Search for size of structure.
+    with open(scale_file, "r") as f:
+        csv_file = csv.reader(f)
+        for row in csv_file:
+            if row[0] == uniprot:
+                scale = row[1]
+                return scale
+
+    # Structure not found in the scale file -> no available.
+    return "Error: No structure available for this UniProtID."
+
+
 ### DATA ROUTES###
 
 ###RECEIVE INCOMING WEBSOCKET MSG FROM NODE.JS
@@ -405,7 +445,7 @@ def nodepanel():
     add_key = "NA"  # Additional key to show under Structural Information
     # nodes = {node["id"]: node for node in nodes}
     global sessionData
-    
+
     if pfile:
         if "ppi" in pfile["name"].lower():
             try:
@@ -416,12 +456,12 @@ def nodepanel():
             uniprots = nodes["nodes"][id].get("uniprot")
             if uniprots:
                 room = session.get("room")
-                #sessionData["actStruc"] = uniprots[0]
+                # sessionData["actStruc"] = uniprots[0]
                 x = '{"id": "prot", "val":[], "fn": "prot"}'
                 data = json.loads(x)
                 data["val"] = uniprots
                 print(data)
-                socketio.emit('ex', data, namespace = '/chat' , room=room)
+                socketio.emit("ex", data, namespace="/chat", room=room)
             # data = names["names"][id]
             return render_template(
                 "nodepanelppi.html",
@@ -542,4 +582,4 @@ def left(message):
 
 
 if __name__ == "__main__":
-    socketio.run(app, port=3000, debug=True)
+    socketio.run(app, debug=True)
