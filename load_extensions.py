fdc1f2bbe8001d17347310fe8c15b50814af8e21
ObT1337 <a12048115@unet.univie.ac.at>
2022-11-09 14:15:19 +0100

    color coded output

--- load_extensions.py
+++ load_extensions.py
@@ -15,12 +15,13 @@ def load(main_app: flask.Flask):
                 main_app.register_blueprint(
                     module.blueprint, url_prefix=module.url_prefix
                 )
-                print(f"Loaded extension: {ext}")
+                print(f"\033[1;32mLoaded extension: {ext}")
             except ImportError as e:
-                print(f"Failed to load extension: {ext}")
-                print(f"Make sure you installed a necessary python modules.")
+                print(f"\u001b[33mFailed to load extension: {ext}")
+                print(f"\u001b[33mMake sure you installed a necessary python modules.")
                 print(
-                    f"You can use:\n\npython3 -m pip install -r {ext}/requirements.txt\n\nTo install all requirements"
+                    f"\u001b[33mYou can use:\n\npython3 -m pip install -r {ext}/requirements.txt\n\nTo install all requirements."
                 )
-                print(e)
+                print(f"Excepted an ImportError:{e}\n")
+    print("\n\n\033[1;32mFinished loading extensions, server is running...")
     return main_app
