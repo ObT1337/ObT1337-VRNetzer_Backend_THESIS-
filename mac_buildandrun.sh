e5dbab42ac07a7f88945e3fe18bc9e107bc71491
ObT1337 <a12048115@unet.univie.ac.at>
2022-11-14 23:08:07 +0100

    corrected error with session minor changes: - mac_buildandrun

--- mac_buildandrun.sh
+++ mac_buildandrun.sh
@@ -1,9 +1,13 @@
 #!bash
 python3 -m venv venv
 source venv/bin/activate
+python -m pip install -r extensions/StringEx/requirements.txt
+python -m pip install -r extensions/StringEx/cartographs_requirements.txt
+python -m pip install -r extensions/ProteinStructureFetch/requirements.txt
+# python -m pip install -i https://test.pypi.org/simple/ vrprot
 python -m pip install -r requirements.txt
 export FLASK_APP=app.py
 export FLASK_DEBUG=1
 export FLASK_RELOAD=1
-flask run --with-threads --host=0.0.0.0 --port 3000
+flask run --with-threads --reload --host=0.0.0.0 --port 3000
 
