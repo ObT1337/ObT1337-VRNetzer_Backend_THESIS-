f94cd7c37f528fd4be6cb49c2767ab24ee3681de
ObT1337 <a12048115@unet.univie.ac.at>
2022-11-14 13:22:35 +0100

    removed Extension requirements

--- dev_mac_buildandrun.sh
+++ dev_mac_buildandrun.sh
@@ -1,8 +1,6 @@
 #!bash
 python3 -m venv venv
 source venv/bin/activate
-python -m pip install -r extensions/StringEx/requirements.txt
-python -m pip install -r extensions/StringEx/cartographs_requirements.txt
 python -m pip install -r requirements.txt
 export FLASK_APP=app.py
 export FLASK_DEBUG=1
