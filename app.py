d42d0c70ad4a3884b705029353419da0d22ae345
ObT1337 <a12048115@unet.univie.ac.at>
2022-11-14 10:49:08 +0100

    prototype of landing page

--- app.py
+++ app.py
@@ -455,5 +455,21 @@ def left(message):
     )
 
 
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+
+@app.route("/home")
+def home():
+    links = []
+    for rule in app.url_map.iter_rules():
+        # Filter out rules we can't navigate to in a browser
+        # and rules that require parameters
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+    # links is now a list of url, endpoint tuples
+    return render_template("home.html", links=json.dumps(links))
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app,port=3000, debug=True)
