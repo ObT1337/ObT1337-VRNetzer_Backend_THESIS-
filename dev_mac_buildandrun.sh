e8f53976ded82056e8e04fd6ca3de5152a8f5940
ObT1337 <a12048115@unet.univie.ac.at>
2022-11-14 11:15:40 +0100

    dev startup scripts Merge branch 'main' of https://github.com/menchelab/VRNetzer_Backend into StringEx

---
+++ dev_mac_buildandrun.sh
@@ -0,0 +1,11 @@
+#!bash
+python3 -m venv venv
+source venv/bin/activate
+python -m pip install -r extensions/StringEx/requirements.txt
+python -m pip install -r extensions/StringEx/cartographs_requirements.txt
+python -m pip install -r requirements.txt
+export FLASK_APP=app.py
+export FLASK_DEBUG=1
+export FLASK_RELOAD=1
+flask run --host=0.0.0.0 --port 3000
+
