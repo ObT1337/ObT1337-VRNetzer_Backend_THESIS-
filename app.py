6f01bf936009998bb0871b764dfdcdc3e9e46633
ObT1337 <a12048115@unet.univie.ac.at>
2022-11-21 12:57:46 +0100

    If project is not in list of projects, take the first in list

--- app.py
+++ app.py
@@ -95,6 +95,9 @@ def nodepanel():
     if project is None:
         project = GD.sessionData.get("actPro","new_ppi")
 
+    if project not in GD.sessionData["proj"]:
+        project = GD.sessionData["proj"][0]
+        
     folder = os.path.join("static", "projects", project)
     with open(os.path.join(folder, "pfile.json"), "r") as json_file:
         GD.pfile = json.load(json_file)
