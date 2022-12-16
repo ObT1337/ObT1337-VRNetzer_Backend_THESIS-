7b7ea074830f6ebce4c53459dee41707f72b9da9
ObT1337 <a12048115@unet.univie.ac.at>
2022-12-16 14:38:47 +0100

    Added cv2 to script

--- reduce_size.py
+++ reduce_size.py
@@ -3,12 +3,13 @@ import ntpath
 import os
 import sys
 
+import cv2
 from PIL import Image
 
 # requires pillow 9.0.1+
 
 
-def main(source, dest):
+def main(source, dest,method):
     ext = ["*.bmp", "*.png"]
     for e in ext:
         files = os.path.join(source, "**", e)
@@ -18,12 +19,26 @@ def main(source, dest):
             sub_dir = head.split(os.sep)[-1]
             if "xyz" in head:
                 sub_dir = os.path.join(head.split(os.sep)[-2], sub_dir)
-            image = Image.open(file)
-            image = image.resize((256, 256), Image.Dither.NONE)
+
+            # resized
+            if method == "pillow":
+                image = Image.open(file)
+                image = image.resize((256, 256), Image.Dither.NONE)
+            elif method == "opencv":
+                img = cv2.imread(file)
+                resized = cv2.resize(img,(256,256))
+
+
             dest_dir = os.path.join(dest, sub_dir)
             os.makedirs(dest_dir, exist_ok=True)
             file_name = os.path.join(dest_dir, tail)
-            image.save(file_name)
+
+            # save
+            if method == "pillow":
+                image.save(file_name)
+            elif method =="opencv":
+                cv2.imwrite(file_name,resized)
+
 
 
 if __name__ == "__main__":
@@ -32,4 +47,4 @@ if __name__ == "__main__":
     # dest = r"c:\Users\sebastian\Documents\Unreal Projects\VRNetzer_Backend\static\NewMaps"
     src = "/Users/till/Documents/Playground/VRNetzer_Backend/static/maps/cartoons_ss_coloring"
     dest = "/Users/till/Documents/Playground/VRNetzer_Backend/static/maps/cartoons_ss_coloring_256"
-    main(src, dest)
+    main(src, dest,"pillow")
