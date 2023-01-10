4dcda6896e901749c03e1c6eb21b89308ea51c0f
ObT1337 <a12048115@unet.univie.ac.at>
2023-01-10 17:50:40 +0100

    added another method to reduce image size

--- reduce_size.py
+++ reduce_size.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 import cv2
+import numpy as np
 from PIL import Image
 
 # requires pillow 9.0.1+
@@ -22,24 +23,40 @@ def main(source, dest,method):
 
             # resized
             if method == "pillow":
-                image = Image.open(file)
-                image = image.resize((256, 256), Image.Dither.NONE)
+                img = Image.open(file)
+                img = img.resize((256, 256), Image.Dither.NONE)
             elif method == "opencv":
                 img = cv2.imread(file)
                 resized = cv2.resize(img,(256,256))
-
+            elif method == "skip":
+                img = Image.open(file)
+                img = skip_every_2(img)
 
             dest_dir = os.path.join(dest, sub_dir)
             os.makedirs(dest_dir, exist_ok=True)
             file_name = os.path.join(dest_dir, tail)
 
             # save
-            if method == "pillow":
-                image.save(file_name)
+            if method in ["pillow","skip"]:
+                img.save(file_name.replace("v4","v2"))
             elif method =="opencv":
                 cv2.imwrite(file_name,resized)
 
-
+def skip_every_2(img):
+    res = np.zeros((256,256,3), dtype=np.uint8)
+    tmp =np.array(img)
+    row_idx = 0
+    pixel_idx = 0
+    i = 0
+    new_image =[]
+    for row in range(0, len(tmp)):
+        for pixel in range(0,len(tmp[row,:]),4):
+            res[row_idx, pixel_idx] = tmp[row, pixel]
+            pixel_idx += 1
+            if pixel_idx > 255:
+                row_idx += 1
+                pixel_idx = 0
+    return Image.fromarray(res)
 
 if __name__ == "__main__":
     # main(sys.argv[1], sys.argv[2])
@@ -47,4 +64,4 @@ if __name__ == "__main__":
     # dest = r"c:\Users\sebastian\Documents\Unreal Projects\VRNetzer_Backend\static\NewMaps"
     src = "/Users/till/Documents/Playground/VRNetzer_Backend/static/maps/cartoons_ss_coloring"
     dest = "/Users/till/Documents/Playground/VRNetzer_Backend/static/maps/cartoons_ss_coloring_256"
-    main(src, dest,"pillow")
+    main(src, dest,"skip")
