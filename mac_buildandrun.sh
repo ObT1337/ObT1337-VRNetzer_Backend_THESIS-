fd30919b1eff268fa1dfe590ff4e23b87c6c33e0
ObT1337 <a12048115@unet.univie.ac.at>
2023-02-13 19:47:41 +0100

    Updated Submodule and added Projects

--- mac_buildandrun.sh
+++ mac_buildandrun.sh
@@ -1,12 +1,12 @@
 #!bash
 python3 -m venv venv
 source venv/bin/activate
-python -m pip install -r extensions/StringEx/requirements.txt
-python -m pip install -r extensions/StringEx/cartographs_requirements.txt
-python -m pip install cartoGRAPHs
-python -m pip install -r extensions/ProteinStructureFetch/requirements.txt
+python -m pip install -r extensions/StringEx/requirements.txt | grep -v 'already satisfied'
+python -m pip install -r extensions/StringEx/cartographs_requirements.txt | grep -v 'already satisfied'
+python -m pip install cartoGRAPHs | grep -v 'already satisfied'
+python -m pip install -r extensions/ProteinStructureFetch/requirements.txt | grep -v 'already satisfied'
 # python -m pip install -i https://test.pypi.org/simple/ vrprot
-python -m pip install -r requirements.txt
+python -m pip install -r requirements.txt | grep -v 'already satisfied'
 export FLASK_APP=app.py
 export FLASK_DEBUG=1
 export FLASK_RELOAD=1
